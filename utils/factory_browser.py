81f10e7feea432cd99c8b37c0471a87ef020a960
Vitalii <Repz2010@gmail.com>
2022-08-31 00:48:56 +0300

    Add selenoid preferences (#1)
    * Added preferences for selenoid
    
    Co-authored-by: Vitalii Kuzminov <vitalii.k@pixellu.com>

--- utils/factory_browser.py
+++ utils/factory_browser.py
@@ -6,12 +6,21 @@ from webdriver_manager.chrome import ChromeDriverManager
 def get_driver():
     options = webdriver.ChromeOptions()
     options.add_experimental_option('excludeSwitches', ['enable-automation'])
-    if os.getenv('HEADLESS'):
-        options.add_argument('--headless')
 
-    driver_browser = webdriver.Chrome(
-        ChromeDriverManager().install(),
-        options=options
-    )
+    if os.getenv('SELENOID'):
+        capabilities = {
+            "browserName": "chrome",
+            "browserVersion": "104.0",
+            "selenoid:options": {
+                "enableVNC": True,
+                "enableVideo": False
+            }
+        }
+        driver_browser = webdriver.Remote('http://localhost:4444/wd/hub', options=options, desired_capabilities=capabilities)
+    else:
+        if os.getenv('HEADLESS'):
+            options.add_argument('--headless')
+        driver_browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+
     driver_browser.set_window_size(1920, 1080)
     return driver_browser
