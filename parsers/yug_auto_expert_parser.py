becfe3798043463b609e1c44eaa9e5c43a739b5d
Vitalii Kuzminov <vitalii.k@pixellu.com>
2022-08-31 21:56:00 +0300

    Added paths

--- parsers/yug_auto_expert_parser.py
+++ parsers/yug_auto_expert_parser.py
@@ -1,45 +1,40 @@
 from parsel import Selector
-from utils.helpers import get_model_card_hash, safety_wrapper
+from utils.helpers import get_model_card_hash
 
 
 class YugAutoExpertParser:
+    model_grid_card = '//div[@class="model__grid-card__content--list"]'
     model_cards = '//div[@class="model__grid-card"]'
-    model_image_card = './div[contains(@class, "model__grid-card__head")]/a/img'
-    model_link_card = './div[contains(@class, "model__grid-card__head")]/a'
-    model_power_card = './/span[@class="model__grid-card__content--list-item"]'
+    model_link_card = './/div[contains(@class, "model__grid-card__head")]/a'
+    model_image_card = f'{model_link_card}/img'
     model_price_card = './/div[@class="model__grid-card__content--price_curent"]'
-    model_distance_card = './/span[@class="model__grid-card__content--list-item"]'
-    model_year_card = './/span[@class="model__grid-card__content--list-item"]'
 
     def __init__(self, page_source):
         self.page_source = page_source
 
     def _parse_image(self, card):
-        return safety_wrapper(card.xpath(self.model_image_card).attrib['src'])
+        return card.xpath(self.model_image_card).attrib['src']
 
     def _parse_link(self, card):
-        return safety_wrapper(f"https://yug-avto-expert.ru{card.xpath(self.model_link_card).attrib['href']}")
+        return f"https://yug-avto-expert.ru{card.xpath(self.model_link_card).attrib['href']}"
 
-    def _parse_power(self, card):
-        try:
-            card.xpath(f'{self.model_power_card}/text()')[4].get()
-        except [IndexError, OSError, EOFError, EnvironmentError, ValueError]:
-            return ''
+    def _parse_fuel(self, card):
+        return card.xpath(f'{self.model_grid_card}')[1].xpath('.//span/text()')[0].get()
 
     def _parse_price(self, card):
-        return safety_wrapper(card.xpath(f'{self.model_price_card}/text()').get())
-
-    def _parse_distance(self, card):
-        return safety_wrapper(card.xpath(f'{self.model_distance_card}/text()')[1].get())
+        return card.xpath(f'{self.model_price_card}/text()').get()
 
     def _parse_year(self, card):
-        return safety_wrapper(card.xpath(f'{self.model_year_card}/text()')[0].get())
+        return card.xpath(f'{self.model_grid_card}')[0].xpath('.//span/text()')[0].get()
+
+    def _parse_distance(self, card):
+        return card.xpath(f'{self.model_grid_card}')[0].xpath('.//span/text()')[1].get()
 
     def _parse_helper(self, card):
         return {
             'image': self._parse_image(card),
             'link': self._parse_link(card),
-            'power': self._parse_power(card),
+            'power': self._parse_fuel(card),
             'price': self._parse_price(card),
             'distance': self._parse_distance(card),
             'year': self._parse_year(card)
@@ -50,6 +45,8 @@ class YugAutoExpertParser:
         model_cards = selector.xpath(self.model_cards)
         result_pars_cars = []
         for card in model_cards:
+            if 'Продать автомобиль' or 'Рассчитайте ежемесячный платеж' in card.xpath('./div').extract()[0]:
+                continue
             params = self._parse_helper(card)
             params['hash'] = get_model_card_hash(params)
             result_pars_cars.append(params)
