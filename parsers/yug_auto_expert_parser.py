c370295331a1d7ec4d4b178d0f2d34a24323852e
Vitalii Kuzminov <vitalii.k@pixellu.com>
2022-09-01 22:28:30 +0300

    Added fix for parse method

--- parsers/yug_auto_expert_parser.py
+++ parsers/yug_auto_expert_parser.py
@@ -3,11 +3,12 @@ from utils.helpers import get_model_card_hash
 
 
 class YugAutoExpertParser:
-    model_grid_card = '//div[@class="model__grid-card__content--list"]'
     model_cards = '//div[@class="model__grid-card"]'
+    model_grid_card = './/div[@class="model__grid-card__content--list"]'
     model_link_card = './/div[contains(@class, "model__grid-card__head")]/a'
     model_image_card = f'{model_link_card}/img'
     model_price_card = './/div[@class="model__grid-card__content--price_curent"]'
+    invalid_card_xpath = './/div[contains(@class, "available__grid-item")]'
 
     def __init__(self, page_source):
         self.page_source = page_source
@@ -45,7 +46,8 @@ class YugAutoExpertParser:
         model_cards = selector.xpath(self.model_cards)
         result_pars_cars = []
         for card in model_cards:
-            if 'Продать автомобиль' or 'Рассчитайте ежемесячный платеж' in card.xpath('./div').extract()[0]:
+            invalid_card_raw = card.xpath(self.invalid_card_xpath)
+            if any(invalid_card_raw):
                 continue
             params = self._parse_helper(card)
             params['hash'] = get_model_card_hash(params)
